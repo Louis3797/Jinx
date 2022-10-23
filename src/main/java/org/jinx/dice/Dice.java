c08d3b65afd791b1a65708e78431e7f846c1b233
Louis <louis-kaan2002@web.de>
2022-10-23 22:50:01 +0200

    added dice class

---
+++ src/main/java/org/jinx/dice/Dice.java
@@ -0,0 +1,23 @@
+package org.jinx.dice;
+
+public class Dice {
+
+    /**
+     * Helper Method
+     * Generates a Random number in range of 1-6
+     *
+     * @return Returnes a number in range from 1-6
+     */
+    private int generateRandomNumber() {
+        return (int) (Math.random() * 6) + 1;
+    }
+
+    /**
+     * Returns the outcome of the dice roll
+     *
+     * @return Returns a number in range of 1 to 6
+     */
+    public int use() {
+        return generateRandomNumber();
+    }
+}
