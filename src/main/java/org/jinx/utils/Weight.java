88caf317b2eeeaf519dd46b02e219eeb68921d79
Louis <louis-kaan2002@web.de>
2022-11-11 15:24:16 +0100

    added to String method

--- src/main/java/org/jinx/utils/Weight.java
+++ src/main/java/org/jinx/utils/Weight.java
@@ -1,4 +1,12 @@
 package org.jinx.utils;
 
 public record Weight<T>(T object, int weight) {
+
+    @Override
+    public String toString() {
+        return "Weight{" +
+                "object=" + object.toString() +
+                ", weight=" + weight +
+                '}';
+    }
 }
