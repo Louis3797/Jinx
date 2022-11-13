c6d1b1865817e43fad3fc2166f34e6463ab002e9
Kerim <94744639+Skrpop@users.noreply.github.com>
2022-11-13 17:01:09 +0100

    Update PlayerTest.java

--- src/test/java/org/jinx/player/PlayerTest.java
+++ src/test/java/org/jinx/player/PlayerTest.java
@@ -1,6 +1,5 @@
 package org.jinx.player;
 import org.jinx.card.LuckyCardNames;
-import org.jinx.game.PlayerController;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
