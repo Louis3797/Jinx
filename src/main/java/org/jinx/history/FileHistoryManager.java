7ccdf981c72d714d2690ebd1c799592acc7650cc
Louis <louis-kaan2002@web.de>
2022-12-10 17:29:39 +0100

    FileHistoryManager creates History directory now if its not existing

--- src/main/java/org/jinx/history/FileHistoryManager.java
+++ src/main/java/org/jinx/history/FileHistoryManager.java
@@ -1,20 +1,44 @@
 package org.jinx.history;
 
 import org.jinx.game.PlayerManager;
+import org.jinx.game_state.ResourceManager;
+import org.jinx.logging_file_handler.LogFileHandler;
 import org.jinx.player.AutonomousPlayer;
 import org.jinx.player.Player;
-import org.jinx.savestate.ResourceManager;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class FileHistoryManager implements IHistoryManager {
+
+    private static final Logger logger = Logger.getLogger(FileHistoryManager.class.getName());
     private final PlayerManager playerManager = PlayerManager.getPlayerManagerInstance();
 
     public FileHistoryManager() {
 
+        LogFileHandler logFileHandler = LogFileHandler.getInstance();
+        logger.addHandler(logFileHandler.getFileHandler());
+        logger.setUseParentHandlers(false);
+
+        // Check if dir Histories exists
+        Path path = Paths.get("Histories");
+
+        if (!Files.exists(path)) {
+            logger.warning("Ordner Histories existiert nicht");
+            try {
+                Files.createDirectory(path);
+                logger.info("Histories Ordner wurde angelegt");
+            } catch (IOException ex) {
+                logger.severe(ex.getMessage());
+            }
+        }
     }
 
     @Override
@@ -28,7 +52,7 @@ public class FileHistoryManager implements IHistoryManager {
             fileName += player.getName();
 
             if (!player.isHuman()) {
-                fileName += ((AutonomousPlayer) player).getDifficulty().name();
+                fileName += "-" + ((AutonomousPlayer) player).getDifficulty().name();
             }
 
             fileName += ".txt";
@@ -54,21 +78,22 @@ public class FileHistoryManager implements IHistoryManager {
 
     @Override
     public List<PlayerHistory> getHistory(Player player) {
-        List<PlayerHistory> result;
 
         String fileName = "Histories/";
 
         fileName += player.getName();
 
         if (!player.isHuman()) {
-            fileName += ((AutonomousPlayer) player).getDifficulty().name();
+            fileName += "-" + ((AutonomousPlayer) player).getDifficulty().name();
         }
 
         fileName += ".txt";
 
+        // load Histories
+        // Note that ResourceManager.load can return null
+        // so result can be null
+        List<PlayerHistory> result = (List<PlayerHistory>) ResourceManager.load(fileName);
 
-        result = (List<PlayerHistory>) ResourceManager.load(fileName);
-
-        return result;
+        return result != null ? result : new ArrayList<>();
     }
 }
