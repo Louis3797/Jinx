e031c2b87e3ef4fb720a6bc4855fee271bda96c8
Louis <louis-kaan2002@web.de>
2022-12-12 14:40:03 +0100

    added standard mvp interfaces

---
+++ src/main/java/org/jinx/presenter/IPresenter.java
@@ -0,0 +1,43 @@
+package org.jinx.presenter;
+
+import org.jinx.model.IModel;
+import org.jinx.view.IView;
+
+/**
+ * Standard Generic Presenter Interface
+ * <p>
+ * All Presenter Interfaces should inherit from this interface
+ *
+ * @param <P> Presenter Type
+ * @param <V> View Type
+ */
+public interface IPresenter<P extends IPresenter<P, V>, V extends IView<P, V>> {
+
+    /**
+     * Returns the current view
+     *
+     * @return Returns the current view
+     */
+    V getView();
+
+    /**
+     * Sets View of the Presenter
+     *
+     * @param view View
+     */
+    void setView(IView<P, V> view);
+
+    /**
+     * Returns the Model Object of the Presenter
+     *
+     * @return Returns the Model the presenter uses
+     */
+    IModel getModel();
+
+    /**
+     * Sets the Model of the Presenter
+     *
+     * @param model Model
+     */
+    void setModel(IModel model);
+}
