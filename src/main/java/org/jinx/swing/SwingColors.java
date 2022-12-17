5281e272d5d1094501fbb47319c04c3013cd09de
jrau1801 <janrau1802@gmail.com>
2022-12-17 15:10:15 +0100

    new color

--- src/main/java/org/jinx/swing/SwingColors.java
+++ src/main/java/org/jinx/swing/SwingColors.java
@@ -5,7 +5,8 @@ import java.awt.*;
 public class SwingColors {
 
     public static final Color BackGroundColor = new Color(241, 246, 245);
-    public static final Color BlueColor = new Color(75, 86, 210);
+    //public static final Color BlueColor = new Color(75, 86, 210);
+    public static final Color BlueColor = new Color(207, 107, 169);
     public static final Color LightBlueColor = new Color(173, 216, 230);
     public static final Color TextColor = new Color(24, 24, 24);
     public static final Color SuccesColor = new Color(104, 185, 132);
