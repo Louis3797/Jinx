36b4f5ac71d452af6ded0c0b01f71ec438dbaf80
jrau1801 <janrau1802@gmail.com>
2022-12-18 18:21:46 +0100

    added choice between gui and console

--- src/main/java/org/jinx/Main.java
+++ src/main/java/org/jinx/Main.java
@@ -2,12 +2,20 @@ package org.jinx;
 
 import org.jinx.game.GameController;
 import org.jinx.view.MainView;
+import org.jinx.wrapper.SafeScanner;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new MainView();
-        GameController gc = new GameController();
-        gc.start();
+        SafeScanner safeScanner = new SafeScanner();
+
+        System.out.println("Spiel über GUI oder Konsole laufen lassen?\n[1] GUI\n[2] Konsole");
+        if(safeScanner.nextIntInRange(1,2) == 1){
+            new MainView();
+        }
+        else{
+            GameController gc = new GameController();
+            gc.start();
+        }
     }
 }
